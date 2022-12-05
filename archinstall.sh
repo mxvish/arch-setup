45a97b9e569306dd5afba16c73b0b3baeb6fb85b
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2022-12-05 14:54:10 +0900

    remove identical commands to archsetup.sh

--- archinstall.sh
+++ archinstall.sh
@@ -47,23 +47,3 @@ grub-mkconfig -o /boot/grub/grub.cfg
 passwd
 exit
 reboot
-
-pacman -Syu --noconfirm alsa-utils chrome dmenu fcitx fcitx-configtool fcitx-mozc git i3-wm i3lock i3status man  otf-ipafont sudo vim xfce4-terminal xorg-server xorg-xinit xterm
-echo -e 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-\texec startx
-fi' >> .bash_profile
-
-echo 'exec i3' > .xinitrc
-
-echo 'clear lock
-clear control
-keycode 66 = Control_L
-add control = Control_L Control_R' > .Xmodmap
-
-#enable wifi on startup
-systemctl enable netctl-auto@$(ip r | cut -d " " -f 5 | head -n 1).service
-#hide xfce4-terminal scrollbar by default
-echo 'MiscMenubarDefault=FALSE' >> ~/.config/xfce4/terminal/terminalrc 
-#make display scale larger
-echo 'Xft.dpi: 120' > ~/.Xresources
-xrdb .Xresources 
