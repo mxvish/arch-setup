d1b9403d4cc5e180df08efee44e3f4c6bb30c294
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2023-03-14 22:49:22 +0900

    uncomment around yay installation

--- final.sh
+++ final.sh
@@ -7,10 +7,12 @@ mv vimrc ~/.vimrc
 #make display scale larger
 echo 'Xft.dpi: 120' > ~/.Xresources
 
+:'
 pacman -S --needed --noconfirm base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si --noconfirm
 yay -S --noconfirm brave-bin
+'
 
 reboot
