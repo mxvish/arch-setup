814cce6723faf44227ccffdca362324a538ee20e
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2023-03-17 18:15:48 +0900

    install brave by yay, not flatpak

--- final.sh
+++ final.sh
@@ -5,6 +5,10 @@ mv vimrc ~/.vimrc
 #make display scale larger
 echo 'Xft.dpi: 120' > ~/.Xresources
 
-flatpak install -y brave
+pacman -S --needed --noconfirm base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+yay -S --noconfirm brave-bin
 
 reboot
