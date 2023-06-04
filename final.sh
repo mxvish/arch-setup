30dc5523c45448ac02e188477fb4faf9be7e82ce
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2023-06-04 21:52:50 +0900

    install init.el by wget

--- final.sh
+++ final.sh
@@ -12,7 +12,9 @@ cd yay-bin
 makepkg -si --noconfirm
 yay -S --noconfirm brave-bin
 '
-curl -fLos ~/.emacs.d/init.el --create-dirs https://raw.githubusercontent.com/mxvish/init.el/main/init.el
+wget https://raw.githubusercontent.com/mxvish/init.el/main/init.el
+mkdir ~/.emacs.d/
+mv init.el ~/.emacs.d/
 
 wget https://raw.githubusercontent.com/mxvish/GUI-timer/main/cd.java
 wget https://raw.githubusercontent.com/00xBAD/arch-logo/main/arch_outline.png
