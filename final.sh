da9f63e400b4a7364d3a98a8d1b66708761fccf6
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2023-06-04 15:25:08 +0900

    comment out brave install section

--- final.sh
+++ final.sh
@@ -4,14 +4,14 @@ mv vimrc ~/.vimrc
 
 #make display scale larger
 echo 'Xft.dpi: 120' > ~/.Xresources
-
+:'
 pacman -Syu --needed --noconfirm base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 sudo chown $(whoami):$(whoami) yay-bin
 cd yay-bin
 makepkg -si --noconfirm
 yay -S --noconfirm brave-bin
-
+'
 curl -fLos ~/.emacs.d/init.el --create-dirs https://raw.githubusercontent.com/mxvish/init.el/main/init.el
 
 wget https://raw.githubusercontent.com/mxvish/GUI-timer/main/cd.java
