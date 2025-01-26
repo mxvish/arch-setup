b42e124687b2f67ba72efcdd9b19744eed6b47c6
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-01-26 14:01:50 +0900

    download vimrc from another repo

--- final.sh
+++ final.sh
@@ -4,6 +4,7 @@ chown $USER:$USER /home/$USER/.config/i3/config
 wget -q https://raw.githubusercontent.com/mxvish/i3status/main/i3status.conf
 mv i3status.conf /etc/
 
+wget -q https://raw.githubusercontent.com/mxvish/vimrc/main/vimrc
 mv vimrc /home/$USER/.vimrc
 chown $USER:$USER /home/$USER/.vimrc
 
