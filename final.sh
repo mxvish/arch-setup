1ad75065bfdbe9cf5666ff74a2fc1153b46959ae
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-08-13 21:48:56 +0900

    feat: don't install brave and cd.java

--- final.sh
+++ final.sh
@@ -11,15 +11,6 @@ wget -q https://raw.githubusercontent.com/mxvish/vimrc/main/vimrc
 mv vimrc /home/$USER/.vimrc
 chown $USER:$USER /home/$USER/.vimrc
 
-pacman -Syu --needed --noconfirm base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-sudo chown $(whoami):$(whoami) yay-bin
-cd yay-bin
-makepkg -si --noconfirm
-yay -S --noconfirm brave-bin
-
-#wget https://raw.githubusercontent.com/mxvish/GUI-timer/main/cd.java
-
 pactl set-source-mute @DEFAULT_SOURCE@ toggle
 
 sed -i  's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
