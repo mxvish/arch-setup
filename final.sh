83956a18c44e1bbe018b0300420d5939206b9b17
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-09-05 13:23:16 +0900

    feat: setup sway

--- final.sh
+++ final.sh
@@ -1,6 +1,12 @@
 systemctl enable bluetooth.service 
 systemctl start bluetooth.service 
 
+mkdir -p /home/$HOSTNAME/.config/sway
+cp /etc/sway/config /home/$HOSTNAME/.config/sway
+chown $HOSTNAME:$HOSTNAME /home/$HOSTNAME/.config/sway/config
+
+sed -i '' 's/foot/lxterminal/' /home/$HOSTNAME/.config/sway/config
+
 git clone https://github.com/mxvish/i3config.git
 cd i3config
 mv config /home/$HOSTNAME/.config/i3/
