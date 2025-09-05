8e1495d00e8e1d2af73e230df692f1e86d7df767
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-09-05 12:49:27 +0900

    chore: fix missing space

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ pacman -Syu --noconfirm dhcpcd dialog efibootmgr grub ifplugd iwd os-prober vim
 useradd -G wheel,audio,video -m mxvish
 passwd mxvish
 grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=GRUB
-sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/'/etc/default/grub
+sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="psmouse.synaptics_intertouch=1 quiet snd-hda-intel.model=dell-headset-multi"' >> /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg
