361978a9e5388c91d4172c7b3071a781ba17f591
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-12-13 13:12:22 +0900

    feat: enable full installation

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@
 #exit
 #ip a
 
+isfullinstallation=true
+
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
@@ -67,6 +69,78 @@ EnableNetworkConfiguration=true
 
 [Network]
 NameResolvingService=systemd' > /etc/iwd/main.conf
+
+fullinstall(){
+  packages=(
+  	acpilight
+  	bash-completion
+  	blueman
+  	brightnessctl
+  	btop
+  	dmenu
+   	emacs-nox
+   	exfatprogs
+  	fastfetch
+  	fcitx5
+  	fcitx5-configtool
+  	fcitx5-gtk
+  	fcitx5-mozc
+  	feh
+   	firefox
+   	gcc
+  	git
+  	i3-wm
+  	i3lock
+   	i3status
+   	lxterminal
+  	make
+  	man
+   	nasm
+  	npm
+  	noto-fonts-emoji
+  	otf-ipafont
+  	pipewire
+  	pipewire-pulse
+  	polkit
+    	qemu-desktop
+     	r
+  	ranger
+  	sudo
+   	tree
+  	unzip
+  	wget
+  	xorg-xinit
+   )
+  
+  for i in "${packages[@]}"; do pacman -Syu --noconfirm "$i"; done
+  
+  #echo -e 'if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ] ; then
+  #\texec sway
+  #fi' >> /home/$HOSTNAME/.bash_profile
+  
+  echo 'if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+    exec startx
+  fi' > >> /home/$HOSTNAME/.bash_profile
+  
+  echo 'exec i3' > /home/$HOSTNAME/.xinitrc
+  
+  mv .bashrc /home/$HOSTNAME/
+   
+  mkdir /mnt/usb
+  
+  echo 'Section "InputClass"
+       Identifier "libinput touchpad catchall"
+       MatchIsTouchpad "on"
+       MatchDevicePath "/dev/input/event*"
+       Option "Tapping" "True"
+       Option "TappingDrag" "True"
+       Driver "libinput"
+  EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf
+}
+
+if [ $isfullinstallation ]; then
+    fullinstall();
+fi
 
 exit
 reboot
